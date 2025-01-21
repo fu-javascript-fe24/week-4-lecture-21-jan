8a3b1ae102e5fef6f03075573c14d08daad6a0ff
Jesper Nyberg <91jesnyb@gafe.molndal.se>
2025-01-21 08:55:20 +0100

    added code

---
+++ script.js
@@ -0,0 +1,47 @@
+const log = (msg) => console.log(msg);
+
+log(pokemons);
+
+pageSetup();
+
+function pageSetup() {
+    log('pageSetup()');
+}
+
+function displayActiveSection(event) {
+    log('displayActiveSection()');
+
+    const pokedexSectionRef = document.querySelector('#pokedex');
+    const generateSectionRef = document.querySelector('#generate');
+    const searchSectionRef = document.querySelector('#search');
+}
+
+function setupPokedex() {
+    log('setupPokedex()');
+
+    const pokedexSectionRef = document.querySelector('#pokedexSection');
+}
+function createCard(pokemon) {
+    log('createCard()');
+
+}
+
+function createImg(pokemon) {
+
+}
+
+function createSpan(pokemon) {
+
+}
+
+function createHeading(pokemon) {
+
+}
+
+function createSubHeading(pokemon) {
+
+}
+
+function createCardStat(stat, pokemon) {
+
+}
